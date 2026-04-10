0db88b505f515ba70a0b4c9c0516ebaeaf7351e4
rishavroushan98 <rishavroushan98@gmail.com>
2026-04-11 02:02:50 +0530

    add render deployed backend url

--- frontend/src/util/constant.js
+++ frontend/src/util/constant.js
@@ -2,4 +2,6 @@
 
 // export const BASE_URL = "http://localhost:3000";
 
-export const BASE_URL = "https://coderconnectorbackend.vercel.app";
+// export const BASE_URL = "https://coderconnectorbackend.vercel.app";
+
+export const BASE_URL = "https://coderconnector.onrender.com";
