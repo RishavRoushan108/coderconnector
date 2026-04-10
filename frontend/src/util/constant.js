e4fc296d3b50fe20e738e2d5eca848b8f2e2da76
rishavroushan98 <rishavroushan98@gmail.com>
2026-04-11 00:10:12 +0530

    update backend url

--- frontend/src/util/constant.js
+++ frontend/src/util/constant.js
@@ -1,3 +1,5 @@
 // export const BASE_URL = "/api";
 
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
+
+export const BASE_URL = "https://coderconnectorbackend.vercel.app";
