db76438c231513098d96cb7209c9daf0bbcbe7a4
rishavroushan98 <rishavroushan98@gmail.com>
2026-04-23 08:00:03 +0530

    change base url

--- frontend/src/util/constant.js
+++ frontend/src/util/constant.js
@@ -1,7 +1,7 @@
-// export const BASE_URL = "/api";
+export const BASE_URL = "/api";
 
 // export const BASE_URL = "http://localhost:3000";
 
 // export const BASE_URL = "https://coderconnectorbackend.vercel.app";
 
-export const BASE_URL = "https://coderconnector.onrender.com";
+// export const BASE_URL = "https://coderconnector.onrender.com";
